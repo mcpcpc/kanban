59f7c563af6f02c8f3c5adfef4f60973aa022d74
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2026-03-20 22:50:12 -0400

    update

--- src/kanban/oauth.py
+++ src/kanban/oauth.py
@@ -4,19 +4,7 @@ from quart import session
 from quart import url_for
 from quart import Quart
 from quart import redirect
-from quart import render_template
-from quart import request
 from quart_authlib import OAuth
-
-
-def authorize(view):
-    @wraps(view)
-    async def wrapper(*args, **kwargs):
-        if session.get("user"):
-            return await view(*args, **kwargs)
-        return redirect(url_for("login"))
-
-    return wrapper
 
 
 def init_oauth(app: Quart) -> Quart:
@@ -29,22 +17,30 @@ def init_oauth(app: Quart) -> Quart:
 
     @app.route("/login")
     async def login():
-        return await render_template("login.html")
-
-    @app.route("/login/google")
-    async def login_google():
-        redirect_uri = url_for("authorize", _external=True)
+        redirect_uri = url_for("auth", _external=True)
         return oauth.google.authorize_redirect(redirect_uri)
 
     @app.route("/auth")
-    async def authorize():
+    async def auth():
         token = await oauth.google.authorize_access_token()
         session["user"] = token["userinfo"]
-        return redirect(url_for("index"))
+        return redirect(url_for("console.read"))
 
     @app.route("/logout")
     async def logout():
         session.pop("user", None)
-        return redirect(url_for("login"))
+        return redirect(url_for("console.read"))
 
     return app
+
+
+def authorize(view):
+    """Authorization wrapper method."""
+
+    @wraps(view)
+    async def wrapper(*args, **kwargs):
+        if session.get("user"):
+            return await view(*args, **kwargs)
+        return redirect(url_for("login"))
+
+    return wrapper
