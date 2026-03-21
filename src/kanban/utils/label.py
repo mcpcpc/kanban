a5b11d8dcaf0a895a4da218a598bc2ef7e3b01b4
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2026-03-21 19:11:35 -0400

    update

--- src/kanban/utils/label.py
+++ src/kanban/utils/label.py
@@ -1,17 +1,21 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
 class LabelTemplateBase:
-    id: int
-    part_number: str
-    part_manufacturer: str
-    part_description: str
-    unit_of_measure_abbreviation: str
-    reorder_point: int
-    kanban_quantity: int
-
-    def __post_init__(self) -> None:
+    def __init__(self, 
+        id: int,
+        part_number: str,
+        part_manufacturer: str,
+        part_description: str,
+        unit_of_measure_abbreviation: str,
+        reorder_point: int,
+        kanban_quantity: int,
+        **kwargs
+    ) -> None:
+        self.id = id
+        self.part_number = part_number
+        self.part_manufacturer = part_manufacturer
+        self.part_description = part_description
+        self.unit_of_measure_abbreviation = unit_of_measure_abbreviation
+        self.reorder_point = reorder_point
+        self.kanban_quantity = kanban_quantity
         self.qr = f"K{self.id:06d}"
 
     def render(self) -> str:
